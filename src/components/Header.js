ede852f948e28f3d7a86c2f36711f128bf63c25e
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-04 11:20:12 +0200

    create header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><a href="#About">About</a></li>
+        <li><a href="#Projects">Projects</a></li>
+        <li><a href="#Contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
