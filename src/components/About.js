ee775b5c37573242e1e5fa311e18f32d95d47cd0
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-04 23:01:29 +0200

    update about section

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/About.css';
+
+const About = () => (
+  <section id="About" className="about">
+    <h1>About</h1>
+    <p>
+      I am a full stack web developer with a passion for creating and learning.
+      I have a background in Engineering and have always been interested in technology.
+      I am excited to continue learning and growing as a developer.
+    </p>
+  </section>
+);
+
+export default About;
