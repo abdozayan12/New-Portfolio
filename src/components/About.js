1251970deb46f0c8ddf702224615bf74d1215065
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-05 12:17:18 +0200

    add images

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import '../styles/About.css';
+import aboutImage from '../assets/aboutImage.png';
 
 const About = () => (
   <section id="About" className="about">
-    <h1>About</h1>
-    <p>
-      I am a full stack web developer with a passion for creating and learning.
-      I have a background in Engineering and have always been interested in technology.
-      I am excited to continue learning and growing as a developer.
-    </p>
+    <div className="about-image">
+      <img src={aboutImage} alt="about" />
+    </div>
+    <div className="about-text">
+      <h1>About Me</h1>
+      <p>
+        I am a full stack web developer with a passion for
+        creating interactive and responsive web applications.
+        I have experience working with
+        JavaScript, React, Redux, Ruby, Rails, PostgreSQL, HTML, CSS, and Git.
+        I am a quick learner and I am always looking to expand my knowledge and skill set.
+        I am a team player and I am excited to work with others to create amazing applications.
+      </p>
+    </div>
   </section>
 );
 
