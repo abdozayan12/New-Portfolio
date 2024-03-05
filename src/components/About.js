b2f7efd8e8ecb9a5a10fe66196a24074c57a54a6
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-05 12:55:48 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -18,6 +18,27 @@ const About = () => (
           I am a quick learner and I am always looking to expand my knowledge and skill set.
           I am a team player and I am excited to work with others to create amazing applications.
         </p>
+        <div className="about-buttons">
+          <button type="button" className="skills-button">skills</button>
+          <ul>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>Ruby</li>
+            <li>Rails</li>
+            <li>PostgreSQL</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Git</li>
+          </ul>
+          <button type="button" className="Education-button">Education</button>
+          <ul>
+            <li>Microverse (Remote Full Stack Development program)</li>
+            <li>FreeCodeCamp</li>
+            <li>Faculity of Engineering</li>
+            <li>Codecademy</li>
+          </ul>
+        </div>
       </div>
     </section>
   </div>
