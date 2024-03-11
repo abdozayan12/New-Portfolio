5e9c1b1a8985dd491897329c9db241dec2854a48
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-11 22:07:42 +0200

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,8 +3,27 @@ import React from "react";
 const Contact = () => {
     return (
         <div>
+        <div>
         <h1>Contact</h1>
         <p>Send us a message!</p>
+        </div>
+        <div>
+        <form>
+            <label>
+                Name:
+                <input type="text" name="name" />
+            </label>
+            <label>
+                Email:
+                <input type="text" name="email" />
+            </label>
+            <label>
+                Message:
+                <textarea name="message" />
+            </label>
+            <button type="submit">Send</button>
+        </form>
+        </div>
         </div>
     );
 }
