98f454d7ea1e9f33896659fb01af7ff9e908188d
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-16 12:41:19 +0200

    Update Welcoming.js

--- src/components/Welcoming.js
+++ src/components/Welcoming.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const Welcoming = () => (
-    <section id="Welcome" className="welcome">
-        <h1>Welcome, I am Abdelhakim Zayan</h1>
-        <p>
-        I am a Full Stack Developer with a passion for learning new technologies
-        and creating beautiful and user-friendly web applications. I have
-        experience in building web applications using HTML, CSS, JavaScript, and
-        React. I am looking forward to working with a team of developers and
-        designers to create amazing web applications.
-        </p>
-    </section>
-    );
+  <section id="Welcome" className="welcome">
+    <h1>Welcome, I am Abdelhakim Zayan</h1>
+    <p>
+      I am a Full Stack Developer with a passion for learning new technologies
+      and creating beautiful and user-friendly web applications. I have
+      experience in building web applications using HTML, CSS, JavaScript, and
+      React. I am looking forward to working with a team of developers and
+      designers to create amazing web applications.
+    </p>
+  </section>
+);
 
 export default Welcoming;
