fb7424a99077d5bb195109ecb46a91bd6a66298a
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-18 13:47:32 +0200

    fix linters

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,12 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
 
-const Header = () => {
-  return (
-    <nav className="navBar">
-      <img src="https://www.docplanner.com/img/logo-default-group-en.svg?v=1" alt="Docplanner Group" />
-      <div className="menu">
+const Header = () => (
+  <nav className="navBar">
+    <img src="https://www.docplanner.com/img/logo-default-group-en.svg?v=1" alt="Docplanner Group" />
+    <div className="menu" />
+    <button type="button" className="Contact">Contact Me</button>
+  </nav>
+);
 
-      </div>
-      <button className="Contact">Contact Me</button>
-    </nav>  
-  )
-}
-
-export default Header
-
+export default Header;
