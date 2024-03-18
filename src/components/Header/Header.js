73c250ace8df960f62627aab5b60764de35d99b8
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-18 13:53:12 +0200

    Add Logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Header.css';
+import logo from '../assets/My Logo.jpg';
 
 const Header = () => (
   <nav className="navBar">
-    <img src="https://www.docplanner.com/img/logo-default-group-en.svg?v=1" alt="Docplanner Group" />
+    <img src={logo} alt="Logo" className="logo" />
     <div className="menu" />
     <button type="button" className="Contact">Contact Me</button>
   </nav>
