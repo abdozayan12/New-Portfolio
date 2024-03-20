8a6c1d39ecd3e761a34f5d11ac5be5850669347e
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-20 21:00:39 +0200

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,11 @@ import logo from '../assets/My Logo.jpg';
 const Header = () => (
   <nav className="navBar">
     <img src={logo} alt="Logo" className="logo" />
-    <div className="menu" />
+    <div className="menu">
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#portfolio">Projects</a>
+    </div>
     <button type="button" className="Contact">Contact Me</button>
   </nav>
 );
