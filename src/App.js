0d6608dd78f2ea517a290328620936e6078098d3
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-23 10:45:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header';
+import Intro from './components/intro/intro';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Intro />
     </div>
   );
 }
