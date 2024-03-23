f03aca839526ca1f9540004841ef62623502c10d
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-23 13:15:49 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,23 @@ import './Header.css';
 import logo from '../assets/My Logo.jpg';
 
 const Header = () => (
-  <nav className="navBar">
-    <img src={logo} alt="Logo" className="logo" />
-    <div className="menu">
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#portfolio">Projects</a>
-      <a href="#contact">Contact</a>
-    </div>
-    <button type="button" className="CV">
-      <a href="https://drive.google.com/file/d/1gnCYa93ft0sJdpm4BpnEwZ6ZBRhK8wqJ/view?usp=sharing" target="_blank" rel="noreferrer">Get my CV</a>
-    </button>
-  </nav>
+  <section className="fixed">
+    <nav className="navBar">
+      <div>
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+      <div className="menu">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#portfolio">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+      <div className="CV">
+        <a href="https://drive.google.com/file/d/1gnCYa93ft0sJdpm4BpnEwZ6ZBRhK8wqJ/view?usp=sharing" target="_blank" rel="noreferrer">Get my CV</a>
+      </div>
+    </nav>
+  </section>
+
 );
 
 export default Header;
