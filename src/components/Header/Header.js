b2570e1a30c08e0f3c05ffdcecff974d68847de9
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-23 12:01:25 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,9 @@ const Header = () => (
       <a href="#portfolio">Projects</a>
       <a href="#contact">Contact</a>
     </div>
-    <button type="button" className="CV">Get my CV</button>
+    <button type="button" className="CV">
+      <a href="https://drive.google.com/file/d/1gnCYa93ft0sJdpm4BpnEwZ6ZBRhK8wqJ/view?usp=sharing" target="_blank" rel="noreferrer">Get my CV</a>
+    </button>
   </nav>
 );
 
