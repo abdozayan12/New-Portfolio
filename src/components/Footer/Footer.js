417c3b7cdbc65900ebd9b1166ad7d7e866b98ce3
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-27 10:52:12 +0200

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import logo from '../assets/My Logo.jpg';
+import './Footer.css';
 
 function Footer() {
   return (
