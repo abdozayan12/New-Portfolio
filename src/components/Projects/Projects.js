7ebb622d2a4957285eff40213b1a46f74357c408
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-29 22:21:00 +0200

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const Projects = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Projects = () => (
+  <div />
+);
 
-export default Projects
+export default Projects;
