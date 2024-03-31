19e1ad8a20a5fe1dab6bd9f985d6b99984bb3fc2
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-03-31 22:08:25 +0200

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const About = () => {
-  return (
-    <div>
-      
+const About = () => (
+  <section>
+    <div className="skill-card">
+      <h2>Frontend</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Redux</li>
+      </ul>
     </div>
-  )
-}
+  </section>
+);
 
-export default About
-
+export default About;
