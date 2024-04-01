f6d0deacace89a20f8bea942d871a673b42a66b8
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-04-01 22:51:25 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,10 +18,10 @@ function Header() {
 
         </div>
         <div className="menu">
-          <a href="#home" target="_self">Home</a>
-          <a href="#about" target="_self">About</a>
-          <a href="#portfolio" target="_self">Projects</a>
-          <a href="#contact" target="_self">Contact</a>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#portfolio">Projects</a>
+          <a href="#contact">Contact</a>
         </div>
         <div className="CV">
           <a href="https://drive.google.com/file/d/1gnCYa93ft0sJdpm4BpnEwZ6ZBRhK8wqJ/view?usp=sharing" target="_blank" rel="noreferrer">Get my CV</a>
