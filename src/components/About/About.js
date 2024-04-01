efad6aa2a1a43812748ab1cf7db922fabea65d79
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-04-01 23:11:37 +0200

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,25 +3,27 @@ import './About.css';
 
 const About = () => (
   <section className="about" id="about">
-    <div className="skill-card">
-      <h2>Frontend</h2>
-      <ul className="list">
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>Redux</li>
-      </ul>
-    </div>
-    <div className="skill-card">
-      <h2>Backend</h2>
-      <ul className="list">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>Python</li>
-        <li>Flask</li>
-        <li>RESTful APIs</li>
-      </ul>
+    <div className="about-cards">
+      <div className="skill-card">
+        <h2>Frontend</h2>
+        <ul className="list">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Redux</li>
+        </ul>
+      </div>
+      <div className="skill-card">
+        <h2>Backend</h2>
+        <ul className="list">
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>Python</li>
+          <li>Flask</li>
+          <li>RESTful APIs</li>
+        </ul>
+      </div>
     </div>
   </section>
 );
