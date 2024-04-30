38d3a986dd14894cb557b2b6c446c158e2f3e11a
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-04-30 23:31:24 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,47 +1,34 @@
-import { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import './Header.css';
 import logo from '../assets/My Logo.jpg';
+import './Header.css';
 
-function Header() {
-  const menu = useRef();
-  const [activeLink, setActiveLink] = useState('#home');
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
 
-  const toggleMenu = (e) => {
-    setActiveLink(e.target.getAttribute('href'));
-    menu.current.classList.toggle('active');
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
   };
 
   return (
-    <section className="fixed">
-      <nav className="navBar" ref={menu}>
-        <div>
-          <a href="#home" className="logo"><img src={logo} alt="Logo" className="logo" /></a>
+    <header className="header">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
 
-        </div>
-        <div className="menu">
-          <div className="menu">
-            <a href="#home" className={`menu-link ${activeLink === '#home' ? 'active' : ''}`} onClick={toggleMenu}>Home</a>
-            <a href="#about" className={`menu-link ${activeLink === '#about' ? 'active' : ''}`} onClick={toggleMenu}>About</a>
-            <a href="#Projects" className={`menu-link ${activeLink === '#Projects' ? 'active' : ''}`} onClick={toggleMenu}>Projects</a>
-            <a href="#contact" className={`menu-link ${activeLink === '#contact' ? 'active' : ''}`} onClick={toggleMenu}>Contact</a>
-          </div>
-        </div>
-        <div className="CV">
-          <a href="https://drive.google.com/file/d/1gnCYa93ft0sJdpm4BpnEwZ6ZBRhK8wqJ/view?usp=sharing" target="_blank" rel="noreferrer">Get my CV</a>
-        </div>
-        <div className="menu-icon">
-          <button type="button" className="menuIcon" aria-label="Open menu" onClick={toggleMenu}>
-            <FaBars />
-          </button>
-          <button type="button" className="closeIcon" aria-label="close menu" onClick={toggleMenu}>
-            <FaTimes />
-          </button>
-        </div>
-
-      </nav>
-
-    </section>
+      <button type="button" onClick={toggleMenu} className="menu-icon">
+        {showMenu ? <FaTimes /> : <FaBars />}
+      </button>
+      {showMenu && (
+        <nav className="nav-menu">
+          <a href="#home" className="nav-link">Home</a>
+          <a href="#about" className="nav-link">About</a>
+          <a href="#projects" className="nav-link">Projects</a>
+          <a href="#contact" className="nav-link">Contact</a>
+        </nav>
+      )}
+    </header>
   );
-}
+};
+
 export default Header;
