f330a98ab9750958ee39cab2aef2084b5bb2b329
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-05-05 23:25:52 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,8 +12,19 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="logo-container">
-        <img src={logo} alt="Logo" className="logo" />
+      <div className="desktop-view">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <nav className="nav-menu">
+          <a href="#home" className="nav-link">Home</a>
+          <a href="#about" className="nav-link">About</a>
+          <a href="#projects" className="nav-link">Projects</a>
+          <a href="#contact" className="nav-link">Contact</a>
+        </nav>
+        <div className="CV">
+          <a href="https://docs.google.com/document/d/1izNcOQh23qZgB4AZ7qQ5v9FbVfG60OrYvbnRg8QtJKE/edit?usp=drive_link" target="_blank" rel="noreferrer" className="resume-link">Get My Resume</a>
+        </div>
       </div>
 
       <button type="button" onClick={toggleMenu} className="menu-icon">
